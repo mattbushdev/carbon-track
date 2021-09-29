ed0b628659754815de2ae6cdeda70eaf770af539
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-29 12:02:38 +0100

    remove unused code

--- App.tsx
+++ App.tsx
@@ -1,23 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import React, {useState} from "react";
+import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { withAuthenticator } from "aws-amplify-react-native";
-import Amplify, { Auth } from "aws-amplify";
+import Amplify from "aws-amplify";
 import awsconfig from "./src/aws-exports.js";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-import { ScrollView } from "react-native-gesture-handler";
-import { createUser, getUser } from "./dbfunctions/dynamo.js";
 Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
-import { AmplifyTheme } from "aws-amplify-react-native"
-import { urlSafeEncode } from "@aws-amplify/core";
-
+import { AmplifyTheme } from "aws-amplify-react-native";
 
 const App = () => {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  
+
   if (!isLoadingComplete) {
     return null;
   } else {
@@ -28,17 +24,24 @@ const App = () => {
       </SafeAreaProvider>
     );
   }
-}
+};
 
-const MyButton= Object.assign({}, AmplifyTheme.button, { backgroundColor: '#2F4847', borderRadius: 28, width: '50%', marginLeft: '25%' });
-const MyHeaderText= Object.assign({}, AmplifyTheme.sectionHeaderText, { color: '#2F4847' });
-// const MyPhoto= Object.assign({}, AmplifyTheme.photo, { photo: source={"../src/icons/D7E7E1/bike.png"} }
-const MyHFooterText= Object.assign({}, AmplifyTheme.sectionFooterLink, { color: '#2F4847' });
-const MyTheme = Object.assign({}, AmplifyTheme, { button: MyButton, sectionHeaderText: MyHeaderText, sectionFooterLink: MyHFooterText });
+const MyButton = Object.assign({}, AmplifyTheme.button, {
+  backgroundColor: "#2F4847",
+  borderRadius: 28,
+  width: "50%",
+  marginLeft: "25%",
+});
+const MyHeaderText = Object.assign({}, AmplifyTheme.sectionHeaderText, {
+  color: "#2F4847",
+});
+const MyHFooterText = Object.assign({}, AmplifyTheme.sectionFooterLink, {
+  color: "#2F4847",
+});
+const MyTheme = Object.assign({}, AmplifyTheme, {
+  button: MyButton,
+  sectionHeaderText: MyHeaderText,
+  sectionFooterLink: MyHFooterText,
+});
 
-
-
-export default withAuthenticator(App, {theme: MyTheme});
-
-// export default withAuthenticator(App);
-
+export default withAuthenticator(App, { theme: MyTheme });
